49202c69634645e7d784da9d16811f36e7c97b1d
Zully Chumpitaz Castillo <zully.chumpitaz@gmail.com>
2024-07-05 08:35:01 -0500

    Demo test

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare()
+});
